6059d5e011f16a7fa1ef8f40d1e6ee477866b64c
hamam <hamam.nasrodin@appfuxion.com>
2025-12-11 17:04:42 +0800

    build: switch from dotenv to dotenvy and add .env config
    Add .env file with database and logging configuration
    Replace dotenv dependency with dotenvy for environment variable handling
    Update main.rs to read from .env and add base path routing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use axum::{routing::get, Json, Router};
+use axum::{response::Redirect, routing::get, Json, Router};
+use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
+use std::env;
+const BASE_PREFIX: &str = "/axum-test";
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    dotenv().expect("Failed to read .env");
+    let database_url = match env::var("DATABASE_URL") {
+        Ok(url) => url,
+        Err(_) => "".to_string(),
+    };
+    println!("DATABASE_URL: {}", database_url);
 
-    let app = Router::new().route("/", get(hello_world));
+    let app = Router::new()
+        .route(&format!("{}/", BASE_PREFIX), get(hello_world))
+        // .fallback(handler_redirect);
+        .fallback("Not Found");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap()
 }
 async fn hello_world() -> &'static str {
     return "Hello, world!";
+}
+
+async fn handler_redirect() -> Redirect {
+    Redirect::to(&format!("{}/", BASE_PREFIX))
 }
