4acb669c2a1254358486da6159bed805afd27325
hamam <hamam.nasrodin@appfuxion.com>
2025-12-14 21:02:32 +0800

    feat(handler): add user creation and retrieval endpoints
    Implement basic CRUD operations for users with create and get all endpoints

---
+++ src/handler.rs
@@ -0,0 +1,31 @@
+use crate::models::{CreateUser, User};
+use axum::{extract::State, Json};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+pub async fn create_user(
+    State(pool): State<PgPool>,
+    Json(payload): Json<CreateUser>,
+) -> Json<User> {
+    let user = sqlx::query_as!(
+        User,
+        "INSERT INTO users (id, username, email) VALUES ($1, $2, $3) RETURNING *",
+        Uuid::new_v4(),
+        payload.username,
+        payload.email
+    )
+    .fetch_one(&pool)
+    .await
+    .expect("Create user failed");
+
+    Json(user)
+}
+
+pub async fn get_all_user(State(pool): State<PgPool>) -> Json<Vec<User>> {
+    let users = sqlx::query_as!(User, "SELECT * FROM users")
+        .fetch_all(&pool)
+        .await
+        .expect("Fetch users failed");
+
+    Json(users)
+}
